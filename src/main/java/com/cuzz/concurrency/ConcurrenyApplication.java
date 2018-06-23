d9b022090e34bc1ed2e0ada27336eaeac00ade59
cuzz1 <cuzz1234@163.com>
2018-06-23 17:32:49 +0800

    Initial commit

---
+++ src/main/java/com/cuzz/concurrency/ConcurrenyApplication.java
@@ -0,0 +1,12 @@
+package com.cuzz.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrenyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrenyApplication.class, args);
+	}
+}
