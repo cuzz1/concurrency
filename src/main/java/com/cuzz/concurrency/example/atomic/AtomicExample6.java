4e30bb742e894900f9a12a410d36bdc2a71edddd
cuzz1 <cuzz1234@163.com>
2018-06-24 15:07:09 +0800

    原子性

---
+++ src/main/java/com/cuzz/concurrency/example/atomic/AtomicExample6.java
@@ -0,0 +1,17 @@
+package com.cuzz.concurrency.example.atomic;
+
+import com.cuzz.concurrency.annoations.ThreadSafe;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+@Slf4j
+@ThreadSafe
+public class AtomicExample6 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
