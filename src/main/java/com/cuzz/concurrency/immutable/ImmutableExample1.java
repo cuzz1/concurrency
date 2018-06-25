90088a3252c8181af1b00e2123b7aac06a84b5f4
cuzz1 <cuzz1234@163.com>
2018-06-25 15:48:26 +0800

    不可变类

---
+++ src/main/java/com/cuzz/concurrency/immutable/ImmutableExample1.java
@@ -0,0 +1,29 @@
+package com.cuzz.concurrency.immutable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImmutableExample1 {
+
+    private final static Integer a = 1;
+    private final static String b = "2";
+    private final static Map<Integer, Integer> map = new HashMap<>();
+
+    static {
+        map.put(1, 2);
+        map.put(3, 4);
+        map.put(5, 6);
+    }
+
+    // 可以保证变量不发生变化
+    private void test(final int a) {
+        // a = 1;  // 编译出错
+    }
+
+    public static void main(String[] args) {
+        // a = 2;   // 编译出错
+        // b = "3"; // 编译出错
+        // map = new HashMap<>(); // 编译出错
+        map.put(1, 3);   // 引用对象可以修改里面的值
+    }
+}
---
+++ src/main/java/com/cuzz/concurrency/immutable/ImmutableExample2.java
@@ -0,0 +1,24 @@
+package com.cuzz.concurrency.immutable;
+
+
+import com.cuzz.concurrency.annoations.ThreadSafe;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@ThreadSafe
+public class ImmutableExample2 {
+    private static Map<Integer, Integer> map = new HashMap<>();
+
+    static {
+        map.put(1, 2);
+        map.put(3, 4);
+        map.put(5, 6);
+        map = Collections.unmodifiableMap(map);
+    }
+
+    public static void main(String[] args) {
+        map.put(1, 3); // 运行时报错
+    }
+}
